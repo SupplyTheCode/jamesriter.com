d2b713c9dfb7fe9a15d0e1ed02f7f6c1b43412e3
James Riter <chaos67731@gmail.com>
2020-04-09 12:05:29 -0700

    Remove old code

--- src/app/components/MainNavList/MainNavList.component.ts
+++ src/app/components/MainNavList/MainNavList.component.ts
@@ -4,9 +4,4 @@ import { Component } from "@angular/core";
   selector: "MainNavList",
   templateUrl: "./MainNavList.component.html",
 })
-export class MainNavList {
-  scrollToElement($element): void {
-    console.log($element);
-    $element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-  }
-}
+export class MainNavList {}
